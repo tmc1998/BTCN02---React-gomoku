a10150eceb257714d0c36f82a5fdece5f58315b2
Tran Minh Canh <shiro.tmc@gmail.com>
2020-04-14 17:33:39 +0700

    Updates

---
+++ precache-manifest.85661f731e1202e1d73e83afba3e1495.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64cc734d8af70e248e0764db7cb4d456",
+    "url": "/BTCN02---React-gomoku/index.html"
+  },
+  {
+    "revision": "f2687cadb7a70b924cc2",
+    "url": "/BTCN02---React-gomoku/static/css/main.827d1438.chunk.css"
+  },
+  {
+    "revision": "63bd6cf7bcf4024738e6",
+    "url": "/BTCN02---React-gomoku/static/js/2.6273c943.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BTCN02---React-gomoku/static/js/2.6273c943.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2687cadb7a70b924cc2",
+    "url": "/BTCN02---React-gomoku/static/js/main.d3311deb.chunk.js"
+  },
+  {
+    "revision": "0da2d8f9486f2ee11790",
+    "url": "/BTCN02---React-gomoku/static/js/runtime-main.4787a32a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/BTCN02---React-gomoku/precache-manifest.85661f731e1202e1d73e83afba3e1495.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BTCN02---React-gomoku/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
